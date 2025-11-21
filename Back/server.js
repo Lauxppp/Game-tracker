6664ab7bcaa5559b05d9d6a22ab11cb5a7ac70ed
Lauxppp <lauraxpastran@gmail.com>
2025-11-20 23:40:56 -0500

    Add files via upload

--- Back/server.js
+++ Back/server.js
@@ -1,17 +1,26 @@
-require('dotenv').config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express(); 
-const PORT = process.env.PORT || 3000;
-const MONGOBD_URL = process.env.MONGODB_URL;
+import juegoRoutes from "./routes/juegoRoutes.js";
+import resenaRoutes from "./routes/resenaRoutes.js";
 
+dotenv.config();
+
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(MONGODB_URL) /*/Aquí allá no funciona la parte de connexión a mongoose
-.then(() => {
-    console.log('Conexión exitosa a MongoDB Atlas');
-})
-.catch(err => {
-    console.log('Error de conexión', err.message);
-})
+// Conectar a Mongo
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch((err) => console.error("Error Mongo:", err));
+
+// Rutas
+app.use("/api/juegos", juegoRoutes);
+app.use("/api/resenas", resenaRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
